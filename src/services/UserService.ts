3400630dc8f31ff6b3a3f682bfd22ac81bfa3054
Mary Skywalker <snowdennaruto90@gmail.com>
2021-12-27 22:37:35 -0400

    services

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,2 +1,39 @@
+import { response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepositories } from '../repositories/UsersRepositories';
+
+interface IUserProps {
+    name: string;
+    email: string;
+    password: string;
+}
+
+class UserService{
+    async execute({name, email, password}: IUserProps){
+        const usersRepository = getCustomRepository(UsersRepositories);
+
+        if(!email){
+            return response.status(200).json({ message: "email incorrect" });
+        }
+
+        const userAlreadtExist = await usersRepository.findOne({
+            email,
+        });
+
+        if(userAlreadtExist){
+            return response.status(200).json({ message: "User already exists" });
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            password
+        });
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export { UserService }
