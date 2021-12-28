81e6e5622fecd14129010904020f375f727cd7ab
Mary Skywalker <snowdennaruto90@gmail.com>
2021-12-27 22:42:53 -0400

    controllers

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { UserService } from '../services/UserService';
+
+class UserController{
+    async handle(request: Request, response: Response){
+        const { name, email, password } = request.body;
+
+        const userService = new UserService();
+
+        const user = await userService.execute({ name, email, password});
+
+        return response.json(user);
+    }
+}
